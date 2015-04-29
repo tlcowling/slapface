12c07c63787b7b22c05c7aec10b0a0c50d89d850
Tom Cowling <tom.cowling@gmail.com>
2015-04-29 10:37:46 +0100

    User is by default a posixAccount and has a username (cn)

--- spec/lib/models/user_spec.js
+++ spec/lib/models/user_spec.js
@@ -1,11 +1,22 @@
 var mocha = require('mocha');
-var chai  = require('chai');
+var expect = require('chai').expect;
 var User  = require('../../../lib/models/user.js');
 
 describe('An ldap user', function() {
-   it ('should be a posix account with valid data', function() {
-       var user = new User();
+    it ('should be created with with valid data', function() {
+        var user = new User({
+            "name": 'Tom',
+            "password": "Soon to be hashed"
+        });
 
+        expect(user.name()).to.equal('Tom');
+        expect(user.ldapObjectClass()).to.equal('posixAccount');
+    });
 
-   });
+    it ("should not allow instantiation with anything other than an object containing data", function() {
+    });
+
+    it ("should throw an exception if it is not created with all valid fields", function() {
+
+    });
 });
