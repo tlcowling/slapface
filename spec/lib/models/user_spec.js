59e7e801a52f9ea9c582ab7ef301d9d45cbd8ead
Tom Cowling <tom.cowling@gmail.com>
2015-04-29 12:31:58 +0100

    Use top, person, posixAccount for the users
    - Ensures that uid, uidNumber, gidNumber and homeDirectory are set

--- spec/lib/models/user_spec.js
+++ spec/lib/models/user_spec.js
@@ -15,7 +15,7 @@ describe('Ldap user model', function() {
             expect(user.firstname()).to.equal('Tom');
             expect(user.surname()).to.equal('McGiblets');
             expect(user.emailaddress()).to.equal('tom.mcgiblets@gmail.com');
-            expect(user.ldapObjectClass()).to.equal('posixAccount');
+            expect(user.ldapObjectClass()).to.deep.equal(['top', 'person', 'posixAccount']);
         });
 
         it ("should not allow instantiation with anything other than an object containing data", function() {
@@ -27,7 +27,68 @@ describe('Ldap user model', function() {
 
             expect(invalidUser).to.throw(Error);
         });
+
+        it ("should return a valid LDAP object for ldapjs to consume", function() {
+            var user = new User({
+                "firstname"    : 'Tom',
+                "surname"      : 'McGiblets',
+                "emailaddress" : 'tom.mcgiblets@gmail.com',
+                "password"     : 'Soon to be hashed'
+            });
+
+            var toLdapObject = user.toLdapObject();
+
+            expect(toLdapObject).to.have.deep.property('cn', user.firstname());
+            expect(toLdapObject).to.have.deep.property('sn', user.surname());
+            expect(toLdapObject).to.have.deep.property('email', user.emailaddress());
+            expect(toLdapObject).to.have.deep.property('objectclass[0]', 'top');
+            expect(toLdapObject).to.have.deep.property('objectclass[1]', 'person');
+            expect(toLdapObject).to.have.deep.property('objectclass[2]', 'posixAccount');
+        });
+
+        it ("should by default use the value of the cn in lowercase for the uid of the user", function() {
+            var userAccountWithUidSameAsFirstName = new User({
+                "firstname": 'Tom',
+                "surname": 'McGiblets',
+                "emailaddress": 'tom.mcgiblets@gmail.com',
+                "password": 'Soon to be hashed'
+            });
+
+            expect(userAccountWithUidSameAsFirstName).to.have.deep.property('uid', userAccountWithUidSameAsFirstName.cn.toLowerCase());
+        });
+
+        it ("should allow the user account to overwrite the uid with the attribute username", function() {
+            var userAccountWithDifferentUidToCn = new User({
+                "firstname"    : 'Tom',
+                "surname"      : 'McGiblets',
+                "username"     : 'littletom',
+                "emailaddress" : 'tom.mcgiblets@gmail.com',
+                "password"     : 'Soon to be hashed'
+            });
+
+            expect(userAccountWithDifferentUidToCn).to.have.deep.property('uid', 'littletom');
+        });
+
+        it ("should by default use the value of the uid in the home directory", function() {
+            var homeDirectoryDefaultUser = new User({
+                "firstname" : "Fred",
+                "surname"   : "Giblets"
+            });
+
+            expect(homeDirectoryDefaultUser.homedirectory).to.equal('/home/fred');
+        });
+
+        it ("should allow the value of homeDirectory to be overwritten", function() {
+            var homeDirectoryOverwrittenUser = new User({
+                "firstname" : "Fred",
+                "surname"   : "Giblets",
+                "homeDirectory" : "/home/notnormal"
+            });
+
+            expect(homeDirectoryOverwrittenUser.homedirectory).to.equal('/home/notnormal');
+        });
     });
+
     describe('validation', function() {
         it ("should throw an exception if it is not created with all valid fields", function() {
 
