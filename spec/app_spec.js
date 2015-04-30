a44bc7fcd47c0ab53ea6016c3a563e26d9de7227
Tom Cowling <tom.cowling@gmail.com>
2015-04-30 14:49:47 +0100

    Fix ironically named previous commit

--- spec/app_spec.js
+++ spec/app_spec.js
@@ -12,86 +12,89 @@ describe("Slapface", function() {
                 .get('/version')
                 .expect(200, '1.0.0', done);
         });
-    });
 
-    describe("users", function() {
-        describe("getting list of users", function() {
-            it ("should return a list of users", function(done) {
-                request(app)
-                    .get('/api/v1/users')
-                    .expect(204, [], done);
-            });
-            it ("should return a single specific user")//, function(done) {
+        describe("version 1", function() {
+            describe("when getting users", function() {
+                //it ("should return a list of all users for /", function(done) {
+                //    request(app)
+                //        .get('/api/v1/users')
+                //        .expect(204, [], done);
+                //});
+
+                it ("should return a single specific user when specifying the userid")//, function(done) {
                 //request(app)
                 //    .get('/api/v1/users/bob')
                 //    .expect(200, [], done);
-            //});
-        });
+                //});
+            });
 
 
-        describe("creating users", function() {
-            it ("should create a user")//, function(done) {
+            describe("creating users", function() {
+                it ("should create a user")//, function(done) {
                 //request(app)
                 //    .post('/api/v1/users')
                 //    .expect(500, '', done);
-            //});
+                //});
 
-            it ("should fail when trying to create a user that already exists")//, function(done) {
+                it ("should fail when trying to create a user that already exists")//, function(done) {
                 //request(app)
                 //    .post('/api/v1/users')
                 //    .expect(409, '', done);
-            //});
+                //});
+            });
+
+            describe("deleting a user", function() {
+                it ("should delete a user that exists");
+                it ("should return not found when trying to delete a user that doesnt exists");
+            });
+
+            describe("modifying a user", function() {
+                it ("should modify a user");
+                it ("should warn when modifying a user that doesnt exist");
+                it ("should warn when modification is invalid");
+            })
         });
 
-        describe("deleting a user", function() {
-            it ("should delete a user that exists");
-            it ("should return not found when trying to delete a user that doesnt exists");
-        });
+        describe("groups", function() {
+            describe("getting groups", function() {
+                it ("should return a list of groups");
+                it ("should return a specific group");
+            });
 
-        describe("modifying a user", function() {
-            it ("should modify a user");
-            it ("should warn when modifying a user that doesnt exist");
-            it ("should warn when modification is invalid");
-        })
+            describe("creating a group", function() {
+                it ("should create a group");
+                it ("should return 409 when trying to create a group that already exists");
+            });
+
+            describe("deleting groups", function() {
+                it ("should delete a group that exists");
+                it ("should warn when trying to delete a group that is not there");
+            });
+
+            describe("modifying groups", function() {
+                it ("should modify a specific group that exists");
+                it ("should warn when trying to modify a group that does not exist");
+            });
+
+            describe("adding users to a group", function() {
+                it ("should add a user that exists to a group");
+                it ("should warn if trying to add a user to a group that doesn't exist");
+                it ("should warn when trying to add a user that doesnt exist to a group");
+                it ("should warn when trying to add the same user to a group");
+            });
+
+            describe("removing users from a group", function() {
+                it ("should delete a user from a group");
+                it ("should warn if trying to delete a user from a group that doesn't exist");
+                it ("should warn when trying to delete a user that is not in a group");
+                it ("should warn when trying to remove a user that is not part of a group");
+            });
+
+            describe("returning users for a specific group", function() {
+                it ("should return the users for a specific group");
+            });
+        });
     });
 
-    describe("groups", function() {
-        describe("getting groups", function() {
-            it ("should return a list of groups");
-            it ("should return a specific group");
-        });
 
-        describe("creating a group", function() {
-            it ("should create a group");
-            it ("should return 409 when trying to create a group that already exists");
-        });
-
-        describe("deleting groups", function() {
-            it ("should delete a group that exists");
-            it ("should warn when trying to delete a group that is not there");
-        });
-
-        describe("modifying groups", function() {
-            it ("should modify a specific group that exists");
-            it ("should warn when trying to modify a group that does not exist");
-        });
-
-        describe("adding users to a group", function() {
-            it ("should add a user that exists to a group");
-            it ("should warn if trying to add a user to a group that doesn't exist");
-            it ("should warn when trying to add a user that doesnt exist to a group");
-            it ("should warn when trying to add the same user to a group");
-        });
-
-        describe("removing users from a group", function() {
-            it ("should delete a user from a group");
-            it ("should warn if trying to delete a user from a group that doesn't exist");
-            it ("should warn when trying to delete a user that is not in a group");
-            it ("should warn when trying to remove a user that is not part of a group");
-        });
-
-        describe("returning users for a specific group", function() {
-            it ("should return the users for a specific group");
-        });
-    });
 });
